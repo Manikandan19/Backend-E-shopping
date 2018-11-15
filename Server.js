4c9bbbbad206c3ead6482de227ff01ffa3507a42
Manikandan <mk19.manikandan@gmail.com>
2018-11-15 16:58:07 +0530

    registeration functionality added with a validation by Manikandan M

--- Server.js
+++ Server.js
@@ -2,7 +2,7 @@ var MongoClient = require('mongodb').MongoClient;
 var url = "mongodb://localhost:27017/E-shop";
 var express = require('express');
 var app = express();
-var bodyParser=require('body-parser');
+var bodyParser = require('body-parser');
 
 /*In this Method is using for overcome cross-origin problem */
 app.use(function (req, res, next) {
@@ -28,33 +28,34 @@ app.get('/availableProducts', (req, res) => {
     });
 });
 
-function getUserDetail(userData)
-{
-    MongoClient.connect(url, function (err, db) {
-        if (err) throw err;
-        var dbo = db.db("E-shop");
-        dbo.collection("customers").find({}).toArray(function (err, result) {
-            if (err) throw err;
-            console.log(result);
-            res.send(result);
-            db.close();
-        });
-    });
-}
+
 
 /*In this Method is using to set User Register details  */
 app.post('/register', bodyParser.json(), function (req, res) {
-  MongoClient.connect(url, function(err, db) {
-  if (err) throw err;
-  var dbo = db.db("mydb");
-  var myobj =req.body;
-  dbo.collection("customers").insertOne(myobj, function(err, res) {
-    if (err) throw err;
-    console.log("1 document inserted");
-    db.close();
-  });
-});
-res.send("Data inserted successfully");
+    MongoClient.connect(url, function (err, db) {
+        if (err) throw err;
+        var dbo = db.db("E-shop");
+        var myobj = req.body;
+        dbo.collection("customers").find({ "email": myobj.email }).toArray(function (err, result) {
+            if (err) throw err;
+            data = result;
+            console.log(result);
+
+            if (result == '') {
+                dbo.collection("customers").insertOne(myobj, function (err, res) {
+                    if (err) throw err;
+                    console.log("1 document inserted");
+                    db.close();
+                });
+                res.send("Data inserted successfully");
+            }
+            else if (result[0].email == myobj.email) {
+                db.close();
+                res.send("Data Already Exist...")
+            }
+        });
+    });
+
 });
 
 
